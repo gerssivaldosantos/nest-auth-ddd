a2687e6efbbba22b7a811849087393f4faf23eab
gerssivaldo <canalandows@gmail.com>
2023-08-09 16:46:29 -0300

    chore: remove unnecessary type conversion

--- src/@core/auth/application/use-case/sign-in.use-case.ts
+++ src/@core/auth/application/use-case/sign-in.use-case.ts
@@ -105,7 +105,7 @@ export class SignInUseCase extends UseCase {
         accessToken,
         refreshToken,
         ...resultFindByEmail
-      } as unknown as SignInResultDto
+      }
     }
   }
 }
